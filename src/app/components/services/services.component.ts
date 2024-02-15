d0f826563766fe43d7fe7d6b31d975035ca4d7d0
Thabiso Matsaba <thabisomatsaba96@gmail.com>
2024-02-15 15:57:17 +0200

    Implement responsive design for About page
    Implemented CSS adjustments to ensure that content in the About page is centered and properly aligned on smaller screens. Added media queries to handle screen sizes up to 768px width. Also adjusted margins for better spacing between elements.
    
    Changes:
    - Center-align content in the About page for smaller screens
    - Adjusted margins and spacing between elements
    - Added media queries for responsiveness up to 768px width

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,11 +1,34 @@
 import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ImageModalComponent } from '../image-modal/image-modal.component';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':increment', [
+        style({ opacity: 1 }),
+        animate('500ms', style({ opacity: 0 })),
+      ]),
+      transition(':decrement', [
+        style({ opacity: 1 }),
+        animate('500ms', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('slide', [
+      transition('0 => 1', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('500ms', style({ transform: 'translateX(0%)', opacity: 1 })),
+      ]),
+      transition('0 => -1', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('500ms', style({ transform: 'translateX(0%)', opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class ServicesComponent {
   constructor(private renderer: Renderer2, private elementRef: ElementRef, private dialog: MatDialog) { }
@@ -41,5 +64,43 @@ export class ServicesComponent {
       width: '90%',
       height: '90%',
     });
+  }
+
+  testimonials: any[] = [
+    {
+      quote:
+        'Thabiso consistently demonstrates diligence in his work. He fearlessly delves into new skills, constructing projects to enhance his capabilities. He is committed to solving challenges independently but also knows when to seek assistance. Thabiso\'s adaptability and collaborative nature make a valuable team player in the realm of software development.',
+      image: 'assets/images/Tiyani.jpeg',
+      name: 'Tiyani Baloyi',
+      designation:
+        'Junior Software Engineer, South African Radio and Astronomy Observatory (SARAO)',
+    },
+    {
+      quote:
+        'Thabiso is a hard worker, has a positive attitude, and shows a huge willingness to grow. He works well under pressure.',
+      image: 'assets/images/Luvo.PNG',
+      name: 'Luvo Ntshwela',
+      designation: 'Junior Software Developer, Entelect',
+    },
+    {
+      quote:
+        'Without a doubt, Thabiso is one of the talented programmers, trustworthy, and professional. His advice has helped me grow in the field.',
+      image: 'assets/images/Vuyo.jpg',
+      name: 'Vuyolwethu Mfidi',
+      designation: 'Junior Software Developer, Uvu Africa:CapaCiti',
+    },
+  ];
+
+  selectedIndex = 0;
+
+  ngOnInit(): void {
+    // Automatically switch testimonials every 5 seconds
+    setInterval(() => this.nextTestimonial(), 5000);
+  }
+
+
+  nextTestimonial(): void {
+    const currentIndex = this.testimonials.findIndex((t) => t === this.testimonials[this.selectedIndex]);
+    this.selectedIndex = (currentIndex + 1) % this.testimonials.length;
   }
 }
